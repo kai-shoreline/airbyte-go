311a77a36eb112d02e57a9d7a8435ab2a24df5e2
Kai Tan <kaitan@KaitekiMBP.localdomain>
2023-05-10 13:19:30 -0700

    test

--- Netlify/main.go
+++ Netlify/main.go
@@ -14,20 +14,43 @@ func main() {
 	
 	teamName := "Shoreline"
 	token := "WdxR8VoUhXWRjc7O_KFB2YwMLLpK31BgdkuwduBqCQo"
-
+	domain := "test6-insights.shoreline.io"
 	account := getAccount(token, teamName)
+	
 	account_slug := account[0]
 	account_id := account[1]
-	res := createSite(token)
+	createDnsZone(account_slug, domain, token)
+	res := createSite(account_slug, domain, token)
 	site_id := res["site_id"].(string)
 	// fmt.Printf("%v", site_id)
+	API_BASE_URL := "https://insights-jk.api.project2022-staging.shoreline.io"
+	API_URL := "https://insights-jk.api.project2022-staging.shoreline.io"
+	AUTH0_AUDIENCE := "https://insights-jk.api.project2022-staging.shoreline.io"
+	AUTH0_BASE_URL := "test-insights.shoreline.io"
+	AUTH0_CLIENT_ID := "PRVTJ1MLUgKf9gks3HqcfAriRI7L43mz"
+	AUTH0_CLIENT_SECRET := "1TNtUixWhqkhc5sFcplJI5Pe47DFGRWqNHv2SxSdosEdprXBzfIMLHMC08DDEcV1"
+	AUTH0_DOMAIN := "jk-insights-dev.us.auth0.com"
+	AUTH0_ISSUER_BASE_URL := "https://jk-insights-dev.us.auth0.com"
+	AUTH0_SECRET := "c8dd15225354b1f35c0693b88a9e217bcdf69c8bdd95d133a1944ec4b2f7d147"
+	AWS_INTEGRATION_ENABLED := "false"
+	ENABLE_RUNBOOK := "true"
+	SENTRY_AUTH_TOKEN := "7807ddf1645d4bdb8883eee8821636880b69b7bfbd794fe1ae33b4d861a7fb12"
+	SHORELINE_INTEGRATION_ENABLED := "true"
+
 	
-	createEnvVars(account_id, site_id, "tes231", "tes322e2") 
-
-	// createDnsZone("cioc-shoreline", "", "test5-insights.shoreline.io")
-
-	// createDomain("3774be8e-3f12-4231-b7b9-d79a2363e6d8", "test2-insights.shoreline.io")
-
+	createEnvVars(account_id, site_id, "API_BASE_URL", API_BASE_URL)
+	createEnvVars(account_id, site_id, "API_URL", API_URL)
+	createEnvVars(account_id, site_id, "AUTH0_AUDIENCE", AUTH0_AUDIENCE)
+	createEnvVars(account_id, site_id, "AUTH0_BASE_URL", AUTH0_BASE_URL)
+	createEnvVars(account_id, site_id, "AUTH0_CLIENT_ID", AUTH0_CLIENT_ID)
+	createEnvVars(account_id, site_id, "AUTH0_CLIENT_SECRET", AUTH0_CLIENT_SECRET)
+	createEnvVars(account_id, site_id, "AUTH0_DOMAIN", AUTH0_DOMAIN)
+	createEnvVars(account_id, site_id, "AUTH0_ISSUER_BASE_URL", AUTH0_ISSUER_BASE_URL)
+	createEnvVars(account_id, site_id, "AUTH0_SECRET", AUTH0_SECRET)
+	createEnvVars(account_id, site_id, "AWS_INTEGRATION_ENABLED", AWS_INTEGRATION_ENABLED)
+	createEnvVars(account_id, site_id, "ENABLE_RUNBOOK", ENABLE_RUNBOOK)
+	createEnvVars(account_id, site_id, "SENTRY_AUTH_TOKEN", SENTRY_AUTH_TOKEN)
+	createEnvVars(account_id, site_id, "SHORELINE_INTEGRATION_ENABLED", SHORELINE_INTEGRATION_ENABLED)
 }
 
 func getAccount(token string, teamName string) [2]string {
@@ -44,40 +67,22 @@ func getAccount(token string, teamName string) [2]string {
 		if v.(map[string]interface {})["name"] == teamName {
 			res[0] = v.(map[string]interface {})["slug"].(string)
 			res[1] = v.(map[string]interface {})["id"].(string)
+			return res
 		}		
-		// for i1, v1 := range v.(map[string]interface {}) {
-		// 	if i1 == "name" {
-
-		// 		fmt.Printf("%+v", v1)
-		// 	}
-		// }
-		// fmt.Printf("%+v", index)
-		// fmt.Printf("%+v", value)
-		// if index == "name" {
-		// 	fmt.Println(value)
-		// }
-		//fmt.Printf("%+v", value)
 	}
-	// fmt.Printf("%v", data[0])
-	// for index, value := range data[1].(map[string]interface {}) {
-	// 	if index == "name" {
-	// 		fmt.Println(value)
-	// 	}
-	// 	//fmt.Printf("%+v", value)
-	// }
 	fmt.Printf("%+v", res)
 	return res
 
 }
 
-func createDnsZone(account_slug string, site_id string, name string) {
+func createDnsZone(account_slug string, name string, token string) {
 
 	url := "https://app.netlify.com/access-control/bb-api/api/v1/dns_zones"
-	payload := strings.NewReader(`{
-		"name": "test5-insights.shoreline.io",
-		"account_slug": "cioc-shoreline"
-	}`)
-	body := postAPI(url, payload, "WdxR8VoUhXWRjc7O_KFB2YwMLLpK31BgdkuwduBqCQo")
+	payload := strings.NewReader(fmt.Sprintf(`{
+		"name": "%s",
+		"account_slug": "%s"
+	}`,name, account_slug))
+	body := postAPI(url, payload, token)
 
 	fmt.Println(string(body))
 	var data map[string]interface{}
@@ -85,27 +90,10 @@ func createDnsZone(account_slug string, site_id string, name string) {
 	if err != nil {
 		panic(err)
 	}
-
 	fmt.Printf("%v", data)
 	
 }
 
-func createDomain(site_id string, domain string) {
-	url := "https://app.netlify.com/access-control/bb-api/api/v1/sites/" + site_id
-	payload := strings.NewReader(`{"custom_domain":"test2-insights.shoreline.io"}`)
-
-	body := putAPI(url, payload, "WdxR8VoUhXWRjc7O_KFB2YwMLLpK31BgdkuwduBqCQo")
-
-	fmt.Println(string(body))
-	var data map[string]interface{}
-	err := json.Unmarshal([]byte(body), &data)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("%v", data)
-
-}
 
 func createEnvVars(account_id string, site_id string, key string, value string) {
 	url := "https://app.netlify.com/access-control/bb-api/api/v1/accounts/" + account_id + "/env?site_id=" + site_id
@@ -137,10 +125,11 @@ func createEnvVars(account_id string, site_id string, key string, value string) 
 	
 }
 
-func createSite(token string) map[string]interface{} {
-	url := "https://app.netlify.com/access-control/bb-api/api/v1/cioc-shoreline/sites"
-	payload := strings.NewReader(`{
-	  "account_slug": "cioc-shoreline",
+func createSite(account_slug string, domain string, token string) map[string]interface{} {
+	url := "https://app.netlify.com/access-control/bb-api/api/v1/" + account_slug + "/sites"
+	payload := strings.NewReader(fmt.Sprintf(`{
+	  "account_slug": "%s",
+	  "custom_domain": "%s",
 	  "repo": {
 		  "provider": "github",
 		  "installation_id": 28900764,
@@ -169,7 +158,7 @@ func createSite(token string) map[string]interface{} {
 		  }
 	  ],
 	  "created_via": ""
-  }`)
+  }`, account_slug, domain))
   
   	body := postAPI(url, payload, token)
 
@@ -181,7 +170,6 @@ func createSite(token string) map[string]interface{} {
 	}
 
 	fmt.Printf("%v", data)
-
 	return data
 }
 
@@ -214,35 +202,6 @@ func postAPI(url string, payload io.Reader, token string) []byte {
 	}
 	return body
   }
-
-func putAPI(url string, payload io.Reader, token string) []byte {
-	client := &http.Client {
-	}
-	req, err := http.NewRequest("PUT", url, payload)
-  
-	if err != nil {
-	  fmt.Println(err)
-	  return nil
-	}
-	if token != "" {
-	  req.Header.Add("Authorization", "Bearer " + token) 
-	}
-	req.Header.Add("Content-Type", "application/json")
-  
-	res, err := client.Do(req)
-	if err != nil {
-	  fmt.Println(err)
-	  return nil
-	}
-	defer res.Body.Close()
-  
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-	  fmt.Println(err)
-	  return nil
-	}
-	return body
-}
 
 func getAPI(url string, token string) []byte {
 	client := &http.Client {
