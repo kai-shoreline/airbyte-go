03e3afbef818368b24df62000efde87a61b46450
Kai Tan <kaitan@KaitekiMBP.localdomain>
2023-05-10 11:21:35 -0700

    test

--- Netlify/main.go
+++ Netlify/main.go
@@ -10,15 +10,63 @@ import (
 )
 
 func main() {
-	// token := "WdxR8VoUhXWRjc7O_KFB2YwMLLpK31BgdkuwduBqCQo"
-	// createSite(token)
+	//creat new team at first, and get token and team name
 	
-	
-	// createEnvVars("630fe0bf52cd4b08420e2721", "3774be8e-3f12-4231-b7b9-d79a2363e6d8", "tes2321", "tes3222") 
+	teamName := "Shoreline"
+	token := "WdxR8VoUhXWRjc7O_KFB2YwMLLpK31BgdkuwduBqCQo"
 
-	createDnsZone("cioc-shoreline", "", "test5-insights.shoreline.io")
+	account := getAccount(token, teamName)
+	account_slug := account[0]
+	account_id := account[1]
+	res := createSite(token)
+	site_id := res["site_id"].(string)
+	// fmt.Printf("%v", site_id)
+	
+	createEnvVars(account_id, site_id, "tes231", "tes322e2") 
+
+	// createDnsZone("cioc-shoreline", "", "test5-insights.shoreline.io")
 
 	// createDomain("3774be8e-3f12-4231-b7b9-d79a2363e6d8", "test2-insights.shoreline.io")
+
+}
+
+func getAccount(token string, teamName string) [2]string {
+	url := "https://app.netlify.com/access-control/bb-api/api/v1/accounts"
+	body := getAPI(url, token)
+	// fmt.Println(string(body))
+	var data []interface{}
+	err := json.Unmarshal([]byte(body), &data)
+	if err != nil {
+		panic(err)
+	}
+	var res [2]string
+	for _, v := range data {
+		if v.(map[string]interface {})["name"] == teamName {
+			res[0] = v.(map[string]interface {})["slug"].(string)
+			res[1] = v.(map[string]interface {})["id"].(string)
+		}		
+		// for i1, v1 := range v.(map[string]interface {}) {
+		// 	if i1 == "name" {
+
+		// 		fmt.Printf("%+v", v1)
+		// 	}
+		// }
+		// fmt.Printf("%+v", index)
+		// fmt.Printf("%+v", value)
+		// if index == "name" {
+		// 	fmt.Println(value)
+		// }
+		//fmt.Printf("%+v", value)
+	}
+	// fmt.Printf("%v", data[0])
+	// for index, value := range data[1].(map[string]interface {}) {
+	// 	if index == "name" {
+	// 		fmt.Println(value)
+	// 	}
+	// 	//fmt.Printf("%+v", value)
+	// }
+	fmt.Printf("%+v", res)
+	return res
 
 }
 
@@ -89,7 +137,7 @@ func createEnvVars(account_id string, site_id string, key string, value string) 
 	
 }
 
-func createSite(token string) {
+func createSite(token string) map[string]interface{} {
 	url := "https://app.netlify.com/access-control/bb-api/api/v1/cioc-shoreline/sites"
 	payload := strings.NewReader(`{
 	  "account_slug": "cioc-shoreline",
@@ -133,6 +181,8 @@ func createSite(token string) {
 	}
 
 	fmt.Printf("%v", data)
+
+	return data
 }
 
 
@@ -165,10 +215,39 @@ func postAPI(url string, payload io.Reader, token string) []byte {
 	return body
   }
 
-  func putAPI(url string, payload io.Reader, token string) []byte {
+func putAPI(url string, payload io.Reader, token string) []byte {
 	client := &http.Client {
 	}
 	req, err := http.NewRequest("PUT", url, payload)
+  
+	if err != nil {
+	  fmt.Println(err)
+	  return nil
+	}
+	if token != "" {
+	  req.Header.Add("Authorization", "Bearer " + token) 
+	}
+	req.Header.Add("Content-Type", "application/json")
+  
+	res, err := client.Do(req)
+	if err != nil {
+	  fmt.Println(err)
+	  return nil
+	}
+	defer res.Body.Close()
+  
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+	  fmt.Println(err)
+	  return nil
+	}
+	return body
+}
+
+func getAPI(url string, token string) []byte {
+	client := &http.Client {
+	}
+	req, err := http.NewRequest("GET", url, nil)
   
 	if err != nil {
 	  fmt.Println(err)
